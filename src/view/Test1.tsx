619740f866fbd082ed66c78f8aecb535c0707655
linzhe141 <linzhe141@qq.com>
2022-12-18 19:05:56 +0800

    [add] 1、新增数字滚动组件

--- src/view/Test1.tsx
+++ src/view/Test1.tsx
@@ -1,6 +1,29 @@
+import MenuNav from "@/components/menunav/MenuNav";
 import { useMousemoveHooks } from "../hooks/useMousemoveHooks";
+const menuData = [
+  {
+    name: "test1",
+    level: 1,
+    children: [
+      {
+        name: "test1-1",
+        level: 2,
+        children: [{ name: "test1-1-1", level: 3 }],
+      },
+    ],
+  },
+  { name: "test2", level: 1 },
+  { name: "test3", level: 1 },
+];
 const Test1 = () => {
   const coord = useMousemoveHooks();
-  return <div style={{ height: "600px" }}>{JSON.stringify(coord)}</div>;
+  return (
+    <>
+      <p>递归组件</p>
+      <MenuNav data={menuData} />
+      <p>自定义hooks</p>
+      <div style={{ height: "600px" }}>{JSON.stringify(coord)}</div>
+    </>
+  );
 };
 export default Test1;
